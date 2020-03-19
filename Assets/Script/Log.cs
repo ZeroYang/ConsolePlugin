cc8b025aef9ddf6312c04710a5a0d6f817a69172
ZeroYang <yangtianbo@zwwxgame.com>
2020-03-19 10:44:45 +0800

    console plugin
    Unity 日志屏幕输出Plugin，方便移动设备查看
    使用方法：
    GameObject Console = UnityEngine.Object.Instantiate<GameObject>(Resources.Load<GameObject>("Console"));
    		Console.SetActive(true);

---
+++ Assets/Script/Log.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : MonoBehaviour {
+
+	private GameObject Console;
+	// Use this for initialization
+	void Start () {
+		Console = UnityEngine.Object.Instantiate<GameObject>(Resources.Load<GameObject>("Console"));
+		Console.SetActive(true);
+
+		UnityEngine.Debug.Log("Console plugin");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
